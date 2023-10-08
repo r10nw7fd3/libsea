ccfdc106039f0d8119b640dc6d2eb4342ded974f
r10nw7fd3 <r10nw7fd3@outlook.com>
2023-10-08 19:33:48 +0300

    Create stdbool.h

---
+++ include/stdbool.h
@@ -0,0 +1,12 @@
+#ifndef __LIBSEA_STDBOOL_H__
+#define __LIBSEA_STDBOOL_H__
+
+#ifndef __cplusplus
+#define bool _Bool
+#define true 1
+#define false 0
+#endif
+
+#define __bool_true_false_are_defined 1
+
+#endif
