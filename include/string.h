5479cc670518c62f997d0f01493e5de5f3f75bce
r10nw7fd3 <r10nw7fd3@outlook.com>
2023-10-09 16:57:13 +0300

    Add memchr and strchr functions

--- include/string.h
+++ include/string.h
@@ -4,6 +4,8 @@
 #include <__nomangle.h>
 #include <stddef.h>
 
-NOMANGLE size_t strlen(const char*);
+NOMANGLE size_t strlen(const char* s);
+NOMANGLE void* memchr(const void* s, int c, size_t n);
+NOMANGLE char* strchr(const char* s, int c);
 
 #endif
--- src/string.c
+++ src/string.c
@@ -7,3 +7,23 @@ size_t strlen(const char* str) {
 
 	return str - start;
 }
+
+void* memchr(const void* s, int c, size_t n) {
+	const unsigned char* s_uc = s;
+	for(size_t i = 0; i < n; ++i)
+		if(s_uc[i] == (unsigned char) c)
+			return (void*) &s_uc[i];
+
+	return NULL;
+}
+
+char* strchr(const char* s, int c) {
+	if(!c)
+		return (char*) s + strlen(s);
+
+	for(; *s; ++s)
+		if(*s == (char) c)
+			return (char*) s;
+
+	return NULL;
+}
