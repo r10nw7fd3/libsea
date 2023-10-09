faaa251c344f15a2b639c1ea831b46540f99dc88
r10nw7fd3 <r10nw7fd3@outlook.com>
2023-10-09 16:56:38 +0300

    Add iso646.h

---
+++ include/iso646.h
@@ -0,0 +1,16 @@
+#ifndef __LIBSEA_ISO646_H__
+#define __LIBSEA_ISO646_H__
+
+#define and &&
+#define and_eq &=
+#define bitand &
+#define bitor |
+#define compl ~
+#define not !
+#define not_eq !=
+#define or ||
+#define or_eq |=
+#define xor ^
+#define xor_eq ^=
+
+#endif
