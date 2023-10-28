e620f88c73febdabb77bab5fba06dc448bf8fe8b
r10nw7fd3 <r10nw7fd3@outlook.com>
2023-10-28 11:04:02 +0300

    Define {STDIN, STDOUT, STDERR}_FILENO

--- include/unistd.h
+++ include/unistd.h
@@ -8,6 +8,10 @@
 #define _POSIX_VERSION 200809L
 #define _XOPEN_VERSION 700
 
+#define STDIN_FILENO 0
+#define STDOUT_FILENO 1
+#define STDERR_FILENO 2
+
 NOMANGLE pid_t getpid(void);
 NOMANGLE uid_t getuid(void);
 NOMANGLE gid_t getgid(void);
