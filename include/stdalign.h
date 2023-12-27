dcf37c5f00652818385925ee2cf59ba770f7e3a3
r10nw7fd3 <r10nw7fd3@outlook.com>
2023-12-27 20:00:59 +0300

    Add stdalign.h

---
+++ include/stdalign.h
@@ -0,0 +1,10 @@
+#ifndef __LIBSEA_STDALIGN_H__
+#define __LIBSEA_STDALIGN_H__
+
+#define alignas _Alignas
+#define alignof _Alignof
+
+#define __alignas_is_defined 1
+#define __alignof_is_defined 1
+
+#endif
