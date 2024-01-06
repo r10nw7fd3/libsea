ca359422681894cb2a94e11778a5e4e292be26c3
r10nw7fd3 <r10nw7fd3@outlook.com>
2024-01-07 00:27:43 +0300

    Add stdint.h stub

---
+++ portable/stdint_size_check.c
@@ -0,0 +1,10 @@
+#include <stdint.h>
+
+_Static_assert(sizeof(int8_t) == 1);
+_Static_assert(sizeof(uint8_t) == 1);
+_Static_assert(sizeof(int16_t) == 2);
+_Static_assert(sizeof(uint16_t) == 2);
+_Static_assert(sizeof(int32_t) == 4);
+_Static_assert(sizeof(uint32_t) == 4);
+_Static_assert(sizeof(int64_t) == 8);
+_Static_assert(sizeof(uint64_t) == 8);
