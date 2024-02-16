ad6ee39bcbf624b9022e4207110a888a5072043b
r10nw7fd3 <r10nw7fd3@outlook.com>
2024-02-16 21:15:42 +0300

    Add atoi(), atol(), atoll()

--- portable/include/stdlib.h
+++ portable/include/stdlib.h
@@ -8,4 +8,8 @@ NOMANGLE _Noreturn void _Exit(int status);
 NOMANGLE _Noreturn void abort(void);
 NOMANGLE void* malloc(size_t size);
 
+NOMANGLE int atoi(const char* nptr);
+NOMANGLE long int atol(const char* nptr);
+NOMANGLE long long int atoll(const char* nptr);
+
 #endif
--- portable/stdlib.c
+++ portable/stdlib.c
@@ -1,6 +1,7 @@
 #include <signal.h>
 #include <sys/syscall.h>
 #include <stddef.h>
+#include <ctype.h>
 #include <sys/mman.h>
 
 _Noreturn void _Exit(int status) {
@@ -17,3 +18,34 @@ void* malloc(size_t size) {
 	void* ret = mmap(NULL, size, PROT_EXEC | PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
 	return ret == MAP_FAILED ? NULL : ret;
 }
+
+#define ATOI_FOR_TYPE(type, name) \
+type name(const char* nptr) { \
+	while(isspace(*nptr)) \
+		++nptr; \
+\
+	int negate = 0; \
+	if(*nptr == '-') { \
+		negate = 1; \
+		++nptr; \
+	} \
+	else if(*nptr == '+') { \
+		negate = 0; \
+		++nptr; \
+	} \
+\
+	type ret; \
+	for(ret = 0; *nptr; ++nptr) { \
+		if(!isdigit(*nptr)) \
+			return 0; \
+\
+		ret *= 10; \
+		ret += *nptr - '0'; \
+	} \
+\
+	return negate ? -ret : ret; \
+}
+
+ATOI_FOR_TYPE(int, atoi)
+ATOI_FOR_TYPE(long int, atol)
+ATOI_FOR_TYPE(long long int, atoll)
